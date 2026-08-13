3159af69521585ceeed9be12665f9baf7892fb48
sawyerbernath <shmeeper@gmail.com>
2026-08-12 20:11:30 -0400

    Update provision.zsh

--- provision.zsh
+++ provision.zsh
@@ -11,7 +11,9 @@
 # sudo apt upgrade
 # sudo apt install zsh gh miller jq
 
-# install hledger binary
+yay hledger
+# if the version is up to date, install there.
+# otherwise, install hledger binary:
 cd /usr/local/bin
 curl -LOC- https://github.com/simonmichael/hledger/releases/download/1.34/hledger-linux-x64.tar.gz
 tar xvf hledger-linux-x64.tar.gz && rm -f hledger-linux-x64.tar.gz
@@ -19,22 +21,25 @@ cd
 hledger --version    # should show 1.34
 
 
-# oh-my-zshell and plugins
-# git clone https://github.com BLAH BLAH OH-MY-ZSH REPOOOOO
+wsl# oh-my-zshell and plugins
+# follow instructions at https://github.com/ohmyzsh/ohmyzsh
 rm .bash_logout .bashrc .profile .shell.pre-oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/akash329d/zsh-alias-finder ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-alias-finder  
 git clone --recursive https://github.com/joel-porquet/zsh-dircolors-solarized $ZSH_CUSTOM/plugins/zsh-dircolors-solarized
 git clone https://github.com/robertzk/send.zsh.git $ZSH_CUSTOM/plugins/send
+mkdir /usr/bin/selector
+git clone https://github.com/joknarf/selector.git /usr/bin/selector
 
 # my github repos
-omz reload
+yay github
 gh auth login
 gh repo clone dotfiles
 gh repo clone finance
 rm .zshrc
 ln -s dotfiles/.zshrc
+omz reload
 
 # for chromeos only
 # ln -s /mnt/chromeos/MyFiles/Downloads
